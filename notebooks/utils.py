3eaabe358ca09f9af83b66dbd479d6217944ec88
Guillaume-ds <guillaume.de-surville@hec.edu>
2023-11-08 15:04:30 +0100

    preparation for models

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
 
-def read_data_files():
-    
-    compas_scores_raw = pd.read_csv("../data/raw/archive/compas-scores-raw.csv")
-    cox_violent_parsed_filt = pd.read_csv("../data/raw/archive/cox-violent-parsed_filt.csv")
-    cox_violent_parsed  = pd.read_csv("../data/raw/archive/cox-violent-parsed.csv")
+def read_data_files(frugal:bool=True):
+    """
+    Frugal option to read only the main necessary dataset
+    """
 
-    return compas_scores_raw, cox_violent_parsed_filt, cox_violent_parsed
+    cox_violent_parsed_filt = pd.read_csv("../data/raw/archive/cox-violent-parsed_filt.csv")
+
+    if not frugal:
+        compas_scores_raw = pd.read_csv("../data/raw/archive/compas-scores-raw.csv")
+        cox_violent_parsed  = pd.read_csv("../data/raw/archive/cox-violent-parsed.csv")
+
+        return compas_scores_raw, cox_violent_parsed_filt, cox_violent_parsed
+    
+    return cox_violent_parsed_filt
